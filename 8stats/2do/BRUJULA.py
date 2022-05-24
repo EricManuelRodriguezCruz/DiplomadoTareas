c6c7808c1d00bb744113b68289a5d73f2a269813
EricManuelRodriguezCruz <104727358+EricManuelRodriguezCruz@users.noreply.github.com>
2022-05-23 22:05:24 -0500

    Add files via upload

---
+++ 8stats/2do/BRUJULA.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on sun may 15:30:20 2022
+
+@author: ercru
+"""
+
+from random import choice
+
+def brujula():
+    return choice(['norte', 'sur', 'este', 'oeste'])
+
+
+print(brujula())
+print(brujula())
+print(brujula())
+print(brujula())
